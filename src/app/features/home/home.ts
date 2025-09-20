ac209e5b7fddb27ef0f0188212dacfb2235a59df
victor <victor.torreblanca.franco@vallegrande.edu.pe>
2025-09-19 21:39:56 -0500

    actualizacion home

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,22 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
+import { RouterLink } from '@angular/router'; // 👈 importar esto
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [RouterLink], // 👈 agregarlo aquí
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home implements OnInit {
-  constructor(private meta: Meta, private title: Title) { }
+  constructor(private meta: Meta, private title: Title) {}
 
   ngOnInit(): void {
     this.title.setTitle('Dmaced | Bar de Cocteles');
     this.meta.addTags([
       { name: 'description', content: 'Bar Dmaced - Cocteles, luces cálidas y ambiente único.' },
       { property: 'og:title', content: 'Bar Dmaced' },
-      { property: 'og:description', content: 'Descubre nuestra carta de cocteles en un ambiente con luces cálidas y vibrantes.' },
-      { property: 'og:image', content: 'https://images.unsplash.com/photo-1521017432531-fbd92d768814?q=80&w=1200&auto=format&fit=crop' }
+      { property: 'og:description', content: 'Descubre nuestra carta de cocteles.' },
+      { property: 'og:image', content: '/images/macedfondo.jpg' }
     ]);
   }
 }
